4a041046d3f359ed6d8ff1e263d7cf506d283cb0
jared <dong1013@foxmail.com>
2018-07-12 10:59:00 +0800

    init

---
+++ spring-boot-read-attribute/src/main/java/cn/mariojd/springboot/attribute/AttributeApplication.java
@@ -0,0 +1,12 @@
+package cn.mariojd.springboot.attribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttributeApplication.class, args);
+    }
+}
