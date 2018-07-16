af6a5533ec751b23e33cf1c5b6d54dc409e55f28
jared <dong1013@foxmail.com>
2018-07-16 16:55:58 +0800

    upd

---
+++ spring-boot-shardingjdbc/src/main/java/cn/mariojd/springboot/shardingjdbc/demo/SpringbootShardingjdbcDemoApplication.java
@@ -0,0 +1,14 @@
+package cn.mariojd.springboot.shardingjdbc.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "cn.mariojd.springboot.shardingjdbc.demo.mapper")
+public class SpringbootShardingjdbcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShardingjdbcDemoApplication.class, args);
+    }
+}
---
+++ spring-boot-shardingjdbc/src/main/java/cn/mariojd/springboot/shardingjdbc/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,74 @@
+package cn.mariojd.springboot.shardingjdbc.demo.config;
+
+import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
+import io.shardingsphere.core.api.ShardingDataSourceFactory;
+import io.shardingsphere.core.api.config.ShardingRuleConfiguration;
+import io.shardingsphere.core.api.config.TableRuleConfiguration;
+import io.shardingsphere.core.api.config.strategy.InlineShardingStrategyConfiguration;
+import io.shardingsphere.core.api.config.strategy.StandardShardingStrategyConfiguration;
+import io.shardingsphere.core.util.DataSourceUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * MybatisPlus Config
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Resource
+    private DataSource dataSource;
+
+    @Bean
+    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean() {
+        MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource getShardingDataSource() throws SQLException {
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderTableRuleConfiguration());
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderItemTableRuleConfiguration());
+        shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
+        shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(
+                new InlineShardingStrategyConfiguration("user_id", "ds${user_id % 2}"));
+        shardingRuleConfig.setDefaultTableShardingStrategyConfig(
+                new InlineShardingStrategyConfiguration("order_id", "t_order_$->{order_id % 2}"));
+        shardingRuleConfig.setDefaultTableShardingStrategyConfig(
+                new InlineShardingStrategyConfiguration("order_id", "t_order_item_$->{order_id % 2}"));
+        return ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig,
+                new HashMap<>(0), new Properties());
+    }
+
+    private TableRuleConfiguration getOrderTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration();
+        result.setLogicTable("t_order");
+        result.setActualDataNodes("ds0.t_order${0..1}");
+        result.setKeyGeneratorColumnName("order_id");
+        return result;
+    }
+
+    private TableRuleConfiguration getOrderItemTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration();
+        result.setLogicTable("t_order_item");
+        result.setActualDataNodes("ds0.t_order_item${0..1}");
+        return result;
+    }
+
+    private Map<String, DataSource> createDataSourceMap() {
+        Map<String, DataSource> result = new HashMap<>(1);
+        result.put("ds0", dataSource);
+        return result;
+    }
+
+}
