0c45d8bbe36613fe49560e57283fc22de2093dc4
happyJared <happyjared1995@gmail.com>
2019-04-09 11:00:52 +0800

    feat: init spring-boot-jetcache

---
+++ spring-boot-jetcache/src/main/java/cn/mariojd/jetcache/SpirngBootJetcacheApplication.java
@@ -0,0 +1,13 @@
+package cn.mariojd.jetcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngBootJetcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngBootJetcacheApplication.class, args);
+    }
+
+}
---
+++ spring-boot-jetcache/src/test/java/cn/mariojd/jetcache/SpirngBootJetcacheApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.mariojd.jetcache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpirngBootJetcacheApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
