5a5c9701c0a2ea12e527a5c2366dbccd5782c669
happyJared <happyjared1995@gmail.com>
2019-04-12 16:56:43 +0800

    feat: perfect spring-boot-admin

--- spring-boot-admin/spring-boot-admin-server/src/main/java/cn/mariojd/admin/server/config/SecuritySecureConfig.java
+++ spring-boot-admin/spring-boot-admin-server/src/main/java/cn/mariojd/admin/server/config/SecuritySecureConfig.java
@@ -23,16 +23,21 @@ public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        // 设置redirectTo参数和登录成功重定向地址
         successHandler.setTargetUrlParameter("redirectTo");
         successHandler.setDefaultTargetUrl(adminContextPath + "/");
 
         http.authorizeRequests()
+                // 授予对静态资源和登录页面的公共访问权
                 .antMatchers(adminContextPath + "/assets/**").permitAll()
                 .antMatchers(adminContextPath + "/login").permitAll()
+                // 除上面配置的其他请求都必须经过身份验证
                 .anyRequest().authenticated()
                 .and()
+                // 配置登录和退出请求路径
                 .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
                 .logout().logoutUrl(adminContextPath + "/logout").and()
+                // 启用HTTP-Basic，这是Spring Boot Admin Client注册所必需的
                 .httpBasic().and()
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
@@ -41,4 +46,5 @@ public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
                         adminContextPath + "/actuator/**"
                 );
     }
+
 }
