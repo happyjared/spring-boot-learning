c8b10d6d97f916c6343cb502bd77c5b4ec4d979c
happyJared <happyjared1995@gmail.com>
2019-04-15 11:39:00 +0800

    feat: init druid-spring-boot

---
+++ druid-spring-boot/src/main/java/cn/mariojd/druid/DruidSrpingBootApplication.java
@@ -0,0 +1,13 @@
+package cn.mariojd.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidSrpingBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidSrpingBootApplication.class, args);
+    }
+
+}
