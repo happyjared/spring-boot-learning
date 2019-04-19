8b66bddeeb47940bc3bb98f9084c077ad5bdbc66
happyJared <happyjared1995@gmail.com>
2019-04-19 15:07:13 +0800

    feat: init spring-boot-session-redis

---
+++ spring-boot-admin/src/main/java/cn/mariojd/admin/server/SpringBootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package cn.mariojd.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+}
--- spring-boot-session-redis/src/main/java/cn/mariojd/session/redis/SpringBootSessionRedisApplication.java
+++ spring-boot-session-redis/src/main/java/cn/mariojd/session/redis/SpringBootSessionRedisApplication.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -13,7 +16,7 @@ import java.util.Map;
 import java.util.Random;
 
 @Slf4j
-@RestController
+@Controller
 @SpringBootApplication
 @EnableRedisHttpSession
 public class SpringBootSessionRedisApplication {
@@ -22,21 +25,47 @@ public class SpringBootSessionRedisApplication {
         SpringApplication.run(SpringBootSessionRedisApplication.class, args);
     }
 
-    @GetMapping("login")
-    public Map login(HttpSession session) {
-        if (session.getAttribute("auth") == null) {
-            session.setAttribute("auth", new Random().nextInt(1000000));
+    private static final String ROLE = "Role";
+    private static final String AUTHORIZATION = "Authorization";
+
+    @Resource
+    private HttpSession session;
+
+    @GetMapping
+    public String init() {
+        if (StringUtils.isEmpty(session.getAttribute(AUTHORIZATION))) {
+            return "redirect:/login";
         }
+        return "redirect:/index";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        session.setAttribute(AUTHORIZATION, session.getId());
+        session.setAttribute(ROLE, new Random().nextInt(3));
+        return "redirect:/index";
+    }
+
+    @GetMapping("logout")
+    @ResponseBody
+    public Map logout() {
+        session.removeAttribute(session.getId());
         Map<String, Object> res = new HashMap<>(2);
-        res.put("msg", "登录成功");
+        res.put(AUTHORIZATION, session.getAttribute(AUTHORIZATION));
         return res;
     }
 
-    @GetMapping("api/user/{id}")
-    public Map user(@PathVariable int id, @SessionAttribute String session) {
-        log.info(session);
-        Map<String, Object> res = new HashMap<>(2);
-        res.put("msg", id);
+    @GetMapping("index")
+    @ResponseBody
+    public Map index() {
+        Map<String, Object> res = new HashMap<>(4);
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String s = attributeNames.nextElement();
+            System.out.println("s = " + s);
+        }
+        res.put(ROLE, session.getAttribute(ROLE));
+        res.put(AUTHORIZATION, session.getAttribute(AUTHORIZATION));
         return res;
     }
 
