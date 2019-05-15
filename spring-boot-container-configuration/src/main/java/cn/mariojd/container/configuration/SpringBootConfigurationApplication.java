6184e888bc999f40ee6c122ddff1e610352198bd
Jared <dong1013@foxmail.com>
2019-05-15 20:50:49 +0800

    feat: build spring-boot-container-configuration

--- spring-boot-container-configuration/src/main/java/cn/mariojd/container/configuration/SpringBootConfigurationApplication.java
+++ spring-boot-container-configuration/src/main/java/cn/mariojd/container/configuration/SpringBootConfigurationApplication.java
@@ -2,12 +2,24 @@ package cn.mariojd.container.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.Compression;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
-public class SpringBootConfigurationApplication {
+public class SpringBootConfigurationApplication implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConfigurationApplication.class, args);
+    }
+
+    @Override
+    public void customize(JettyServletWebServerFactory factory) {
+        Compression compression = new Compression();
+        compression.setEnabled(true);
+        compression.setMinResponseSize(DataSize.ofBytes(512));
+        factory.setCompression(compression);
     }
 
 }
