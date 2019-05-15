6114fd8e09006b6f337c6bcc9ef13ee74bf2fc72
Jared <dong1013@foxmail.com>
2019-05-15 17:24:13 +0800

    feat: init spring-boot-container-configuration

---
+++ spring-boot-container-configuration/src/main/java/cn/mariojd/container/configuration/SpringBootConfigurationApplication.java
@@ -0,0 +1,13 @@
+package cn.mariojd.container.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationApplication.class, args);
+    }
+
+}
