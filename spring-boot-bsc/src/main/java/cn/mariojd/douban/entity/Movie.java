87014e9c6466a225520c0066f05ff630b938d488
Jared <dong1013@foxmail.com>
2020-05-29 23:07:02 +0800

    feat: movie

--- spring-boot-bsc/src/main/java/cn/mariojd/douban/entity/Movie.java
+++ spring-boot-bsc/src/main/java/cn/mariojd/douban/entity/Movie.java
@@ -1,12 +1,9 @@
 package cn.mariojd.douban.entity;
 
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Date;
 
 /**
  * @author Jared
